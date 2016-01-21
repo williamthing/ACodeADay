093419a380f5cc25bb6c1366949c01d77aa6f93b
WILLIAM THING <wthing@u.washington.edu>
2016-01-21 13:16:08 -0800

    Changes to solution and adding analysis

--- Java/projects/ReverseLink.java
+++ Java/projects/ReverseLink.java
@@ -1,6 +1,8 @@
 /*
  * William Thing
- * Reverse Linked List
+ * 
+ * 
+ * Reversing a Linked List
  */
 package projects;
 
@@ -14,12 +16,27 @@ public class ReverseLink {
 			current = current.next;
 		}
 		printList(front);
-		reverseList(front);
+		long startTime = System.nanoTime();
+		front = reverseList(front);
+		long endTime = System.nanoTime();
+		System.out.println((endTime - startTime) / Math.pow(10, 6));
+		printList(front);
+		startTime = System.nanoTime();
+		front = reverseList2(front);
+		endTime = System.nanoTime();
+		System.out.println((endTime - startTime) / Math.pow(10, 6));
 		printList(front);
 			
 	}
 	
+	// prints a given linked list with spaces in between its data
 	public static void printList(LinkedListNode front) {
+		printListHelper(front);
+		System.out.println();
+	}
+	
+	// prints a given linked list with spaces in between its data
+	private static void printListHelper(LinkedListNode front) {
 		LinkedListNode current = front;
 		while (current != null) {
 			System.out.print(current.data + " ");
@@ -27,19 +44,50 @@ public class ReverseLink {
 		}
 	}
 	
+	// Returns a given linked list in reversed order
+	// TODO: refactor code (one case not necessary to check)
 	public static LinkedListNode reverseList(LinkedListNode front) {
 		// either front is null or next is null then nothing to reverse
 		if (front == null || front.next == null) {
 			return front;
 		}
-		LinkedListNode last = front;
-		front = front.next;
-		while (front != null) {
-			LinkedListNode tmp = front;
-			front = front.next;
-			tmp.next = last;
-			last = tmp;
+		// We know we have 2 linked list LinkedListNodes at least
+		LinkedListNode second = front.next;
+		LinkedListNode third = second.next;
+		front.next = null;
+		if (third == null) {
+			second.next = front;
+			return second;
 		}
-		return last;
+		
+		second.next = front;
+		front = third;
+		third = front.next;
+		front.next = second;
+		while (third != null) {
+			second = third;
+			third = third.next;
+			second.next = front;
+			front = second;
+		}
+		return front;
+	}
+	
+	// Solution Found Online
+	public static LinkedListNode reverseList2(LinkedListNode node) {
+	    if (node == null || node.next == null) {
+	        return node;
+	    }
+	    LinkedListNode currentNode = node;
+	    LinkedListNode previousNode = null;
+	    LinkedListNode nextNode = null;
+
+	    while (currentNode != null) {
+	        nextNode = currentNode.next;
+	        currentNode.next = previousNode;
+	        previousNode = currentNode;
+	        currentNode = nextNode;
+	    }
+	    return previousNode;
 	}
 }
