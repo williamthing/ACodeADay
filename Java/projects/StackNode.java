0df4c6825eee45138349b0db845c8164a815f226
WILLIAM THING <wthing@u.washington.edu>
2016-01-25 14:26:07 -0800

    stack class

--- Java/projects/StackNode.java
+++ Java/projects/StackNode.java
@@ -1,5 +1,15 @@
 package projects;
 
 public class StackNode {
-
+	public int data;
+	public StackNode next;
+	
+	public StackNode(int n) {
+		this(n, null);
+	}
+	
+	public StackNode(int n, StackNode next) {
+		this.data = n;
+		this.next = next;
+	}
 }
