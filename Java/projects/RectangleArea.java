3a12fb6aee54dda9469247a5973facebf1ab7c0e
WILLIAM THING <wthing@u.washington.edu>
2016-01-29 23:28:49 -0800

    rectangle area

---
+++ Java/projects/RectangleArea.java
@@ -0,0 +1,19 @@
+/* William Thing
+ * Find the total area covered by two rectilinear rectangles in a 2D plane.
+
+Each rectangle is defined by its bottom left corner and top right corner.
+
+Rectangle Area
+Assume that the total area is never beyond the maximum possible value of int.
+ */
+
+package projects;
+
+public class RectangleArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
