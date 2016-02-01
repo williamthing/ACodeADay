7dc5e3ae4c0bd8f221705dd1cd3f902edb5f9430
WILLIAM THING <wthing@u.washington.edu>
2016-02-01 09:24:47 -0800

    c++ practice

---
+++ Cpp/prac.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+#define NAME_SIZE 50
+
+class Person {
+    int id;
+    char name[NAME_SIZE];
+
+public:
+    void aboutMe() {
+        cout << "I am a person." << endl;
+    }
+};
+
+class Student : public Person {
+    public:
+        void aboutMe() {
+            cout << "I am a student." << endl;
+        }
+};
+
+int main() {
+    Student * p = new Student();
+    p->aboutMe();
+    delete p;
+    return 0;
+}
+
+
+
