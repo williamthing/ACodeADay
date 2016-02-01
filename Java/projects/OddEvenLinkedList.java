f741389e5a726d0b6fcaf09a7edc57ed05053897
WILLIAM THING <wthing@u.washington.edu>
2016-01-31 17:08:31 -0800

    revised implementations of problem

--- Java/projects/OddEvenLinkedList.java
+++ Java/projects/OddEvenLinkedList.java
@@ -23,8 +23,10 @@ public class OddEvenLinkedList {
 	public static void main(String[] arg) {
 		// Creating test cases
 		LinkedListNode l1 = null;
+		l1 = oddEvenList(l1);
 		printList(l1);
 		LinkedListNode l2 = new LinkedListNode(10);
+		l2 = oddEvenList(l2);
 		printList(l2);
 		LinkedListNode l3 = new LinkedListNode(15);
 		printList(l3);
@@ -43,27 +45,60 @@ public class OddEvenLinkedList {
 		
 		l3 = oddEvenList(l3);
 		printList(l3);
+		
+		LinkedListNode tmp = l3;
+		l3 = new LinkedListNode(69);
+		l3.next = tmp;
+		printList(l3);
+		// 69->10->22->15->33->17->/
+		l3 = oddEvenList2(l3);
+		printList(l3);
+		l3 = oddEvenList2(l3);
+		printList(l3);
 	
 	}
 	
+	//	arranges nodes so that the odd nodes (not data just node order) come first and the
+	//	even nodes in the original list are last.
 	public static LinkedListNode oddEvenList(LinkedListNode head) {
 		if (head == null || head.next == null || head.next.next == null) return head;
 		LinkedListNode evens = head.next;
 		LinkedListNode currEvens = evens;
 		head.next = head.next.next;
 		LinkedListNode current = head.next;
-		System.out.println(current.data);
 		while (current != null && current.next != null) {
 			currEvens.next = current.next;
 			currEvens = currEvens.next;
-			if (current.next.next == null) break;
-			current = current.next.next;
+			if (current.next.next == null) {
+				break;
+			}
+			current.next = current.next.next;
+			current = current.next;
 		}
 		currEvens.next = null;
 		current.next = evens;
 		return head;
 	}
 	
+	// after looking at someone's solution, trying a more understandable and readable implementation
+	public static LinkedListNode oddEvenList2(LinkedListNode head) {
+		if (head == null) return null;
+		if (head.next == null || head.next.next == null) return head;
+		LinkedListNode oddHead = head;
+		LinkedListNode evenHead = head.next;
+		LinkedListNode oddP = oddHead;
+		LinkedListNode evenP = evenHead;
+		while (evenP != null && evenP.next != null) {
+			oddP.next = oddP.next.next;
+			evenP.next = evenP.next.next;
+			oddP = oddP.next;
+			evenP = evenP.next;
+		}
+		oddP.next = evenHead;
+		return oddHead;
+	}
+	
+	// prints a given list in order from front to last
 	public static void printList(LinkedListNode head) {
 		while (head != null) {
 			System.out.print(head.data + "->");
