9a7a8631f0fad9a8bdaf4dc422ef6157287a88fa
WILLIAM THING <wthing@u.washington.edu>
2016-02-02 15:53:00 -0800

    graph node change

--- Java/projects/GraphNode.java
+++ Java/projects/GraphNode.java
@@ -1,12 +1,15 @@
 package projects;
 
+import java.util.ArrayList;
+
 public class GraphNode {
 	public int data;
 	public boolean visited;
-	public GraphNode[] neighbors;
+	public ArrayList<GraphNode> path;
 	
 	public GraphNode(int n) {
 		this.data = n;
 		this.visited = false;
+		this.path = new ArrayList<GraphNode>();
 	}
 }
