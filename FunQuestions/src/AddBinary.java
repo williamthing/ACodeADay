5a221b1fb562901118739cb22eb1f5ec464d789b
WILLIAM THING <wthing@u.washington.edu>
2016-02-09 19:40:24 -0800

    need to update bugfix

--- FunQuestions/src/AddBinary.java
+++ FunQuestions/src/AddBinary.java
@@ -7,10 +7,9 @@ b = "1"
 Return "100".
  */
 
-
 public class AddBinary {
 	public static void main(String[] arg) {
-		System.out.println(addBinary("11", "1"));
+		System.out.println(addBinary("1", "111"));
 	}
 	
 	public static String addBinary(String a, String b) {
@@ -22,11 +21,47 @@ public class AddBinary {
 		int carryOut = 0;
 		int result = 0;
 		while (len1 >= 0 && len2 >= 0) {
-			result = (a.charAt(len1) - '0') + (b.charAt(len2) - '0');
+			result = (a.charAt(len1) - '0') + (b.charAt(len2) - '0') + carryOut;
 			System.out.println(result);
+			if (result > 1) {
+				carryOut = 1;
+				s.append(result-2);
+			} else {
+				carryOut = 0;
+				s.append(result);
+			}
 			len1--;
 			len2--;
 		}
+		
+		while (len1 >= 0) {
+			result = a.charAt(len1) - '0' + carryOut;
+			System.out.println(result);
+			if (result > 1) {
+				carryOut = 1;
+				s.append(result-2);
+			} else {
+				carryOut = 0;
+				s.append(result);
+			}
+			len1--;
+		}
+		
+		while (len2 >= 0) {
+			result = a.charAt(len2) - '0' + carryOut;
+			System.out.println(result);
+			if (result > 1) {
+				carryOut = 1;
+				s.append(result-2);
+			} else {
+				carryOut = 0;
+				s.append(result);
+			}
+			len2--;
+		}
+		
+		if (carryOut == 1) s.append(carryOut);
+		s = s.reverse();
 		return s.toString();
 	}
 }
