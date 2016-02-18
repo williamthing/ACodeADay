4ae79dcb26261c35c0e471e0bd1aa737f66959c8
WILLIAM THING <wthing@u.washington.edu>
2016-02-18 02:38:53 -0800

    more cool comments\!

--- FunQuestions/src/StringCalculator.java
+++ FunQuestions/src/StringCalculator.java
@@ -18,8 +18,9 @@ public class StringCalculator {
 	// Takes a given string and calculates the result as if the string 
 	// was an equation does not follow paradigm of PEMDAS, only the 
 	// values first seen from left to right
+	// returns negative max integer if given null string or blank string
 	public static int calculateString(String s) {
-		if (s.length() == 0 || s==null) return -1;
+		if (s.length()==0 || s==null) return -1*Integer.MAX_VALUE;
 		char[] cA = s.toCharArray();
 		int start = 0, result = 0, tmp = 0;
 		while (Character.isDigit(cA[start]) && start < cA.length) {
@@ -29,7 +30,7 @@ public class StringCalculator {
 		char operand = 'c';
 		while (start < cA.length) {
 			// get next operand
-			if (start < cA.length && !Character.isDigit(cA[start])) {
+			if (!Character.isDigit(cA[start])) {
 				operand = cA[start++];
 			}
 			// get next val
@@ -45,6 +46,7 @@ public class StringCalculator {
 	
 	// returns result of two given values and a char operand
 	// matching the necessary computation by that given operand
+	// return given result if none of the operand match
 	public static int compute(int result, int next, char operand) {
 		if (operand == '*') return result * next;
 		if (operand == '-') return result - next;
