8b16f355f08a3b3512e6dfeb2ce58596b7a0c779
WILLIAM THING <wthing@u.washington.edu>
2016-02-19 23:37:55 -0800

    insertion sort, stable sort

---
+++ FunQuestions/src/InsertionSort.java
@@ -0,0 +1,33 @@
+// William Thing
+// Insertion Sort
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+	public static void main(String[] arg) {
+		int[] a = {1, 5, 3, 4, 10, 9, 8, 7, 7, 7, 2, 6};
+		insertionSort(a);
+		System.out.println(Arrays.toString(a));
+	}
+		
+	public static void insertionSort(int[] a) {
+		for (int i = 1; i < a.length; i++) {
+			int tmp = a[i];
+			int j = i-1;
+			while (j >= 0 && tmp < a[j]) {
+				a[j+1] = a[j];
+				j--;
+			}
+			a[j+1] = tmp;
+		}
+		
+	}
+		
+	public static void swap(int[] a, int i, int j) {
+		int tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+
+}
