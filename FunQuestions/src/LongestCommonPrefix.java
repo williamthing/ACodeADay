ff489f199d012f3e772617c8990d4aa87c21c7d6
WILLIAM THING <wthing@u.washington.edu>
2016-02-23 00:04:39 -0800

    common prefix

---
+++ FunQuestions/src/LongestCommonPrefix.java
@@ -0,0 +1,30 @@
+// William Thing
+
+public class LongestCommonPrefix {
+	
+	public static void main(String[] arg) {
+		String[] prefix = {	"hill",
+							"hi",
+							"hicky"};
+		String[] noPrefix = {"jim", "jig", "j", "no"};
+		
+	}
+
+    public static String longestCommonPrefix(String[] strings) {
+        int size = strings.length;
+        if (size == 0) return "";
+        StringBuilder s = new StringBuilder(strings[0]);
+        for (int i = 1; i < size; i++) {
+        	while (!strings[i].startsWith(s.toString()))
+        		s.deleteCharAt(s.length()-1);
+        }
+        return s.toString();
+    }
+    
+    public static void truthDetector(boolean bool) {
+    	if (bool)
+    		System.out.println("TRUE");
+    	else
+    		System.out.println("FALSE");
+    }
+}
