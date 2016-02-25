e392730cab238a0c8f95fd73c9a44eb86fc9260a
WILLIAM THING <wthing@u.washington.edu>
2016-02-24 16:23:25 -0800

    updated

--- FunQuestions/src/GroupAnagrams.java
+++ FunQuestions/src/GroupAnagrams.java
@@ -1,17 +1,47 @@
-import java.util.Arrays;
-
 // William Thing
 
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class GroupAnagrams {
+		
+	public void groupAnagrams(String[] words) {
+		HashMap<String, ArrayList<String>> mapList = new HashMap<String, ArrayList<String>>();
+		for (String word : words) {
+			String key = sortChar(word);
+			if (!mapList.containsKey(key)) {
+				ArrayList<String> temp1 = new ArrayList<String>();
+				temp1.add(word);
+				mapList.put(key, temp1);
+			} else {
+				ArrayList<String> temp2 = mapList.get(key);
+				temp2.add(word);
+				mapList.put(key, temp2);
+			}
+		}
+		ArrayList<String> group = new ArrayList<String>();
+		for (String key : mapList.keySet()) {
+			group.addAll(mapList.get(key));
+		}
+		group.toArray(words);
+	}
 	
-	public String[] groupAnagrams(String[] words) {
-		return new String[1];
+	// sort char
+	public String sortChar(String s) {
+		char[] content = s.toCharArray();
+		Arrays.sort(content);
+		return new String(content);
 	}
 	
 	public static void main(String[] arg) {
 		String[] test = {"dog", "horse", "rhose", "god", "food", "odg", "doof", "hi"};
+		String[] test2 = {"dog", "horse", "rhose", "god", "food", "odg", "doof", "hi"};
 		Arrays.sort(test, new AnagramComparator());
-		System.out.println(Arrays.toString(test));	
+		System.out.println(Arrays.toString(test));
+		GroupAnagrams ga = new GroupAnagrams();
+		ga.groupAnagrams(test2);
+		System.out.println(Arrays.toString(test2));
 	}
 
 }
