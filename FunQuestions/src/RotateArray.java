f65475a721d60fb797c15936b0fa637f19dad9ab
WILLIAM THING <wthing@u.washington.edu>
2016-02-25 17:27:07 -0800

    update

--- FunQuestions/src/RotateArray.java
+++ FunQuestions/src/RotateArray.java
@@ -13,39 +13,27 @@ at least 3 different ways to solve this problem.
 
 public class RotateArray {
 	
+	static int count = 0;
+	
 	public static void main(String[] arg) {
 		int[] arr = {1, 2, 3, 4, 5, 6, 7};
 		//int[] arr = {1, 2};
-		rotate(arr, 4);
+		rotate(arr, 2);
 		for (int n : arr) {
 			System.out.print(n);
 		}
 	}
 	
-	/*
-    public static void rotate(int[] nums, int k) {
-    	if (nums == null || nums.length == 1 || k == 0 || k >= nums.length) return;
-    	int i = k, j = 0, tmp;
-        do {
-        	System.out.println(i + " " + j);
-        	tmp = nums[i];
-        	nums[i++] = nums[j];
-        	nums[j++] = tmp;
-        	if (i == nums.length) i = 0;
-        	if (j >= k) j = 0;
-        } while (i != k-1);
-    }
-    */
-	
 	public static void rotate(int[] a, int rotate) {
-	    if (a == null || a.length == 0 || rotate == 0 || rotate > a.length) return;
-		rotateA(a, rotate, 0, rotate-1);
+	    if (a == null || a.length == 0 || rotate < 2 || rotate > a.length) return;
+		rotateA(a, rotate, 0, rotate-1, 1);
 	}
 	
-	private static void rotateA(int[] a, int rotate, int start, int end) {
-		if ((end % a.length) != rotate-2) {
+	private static void rotateA(int[] a, int rotate, int start, int end, int index) {
+		if (index < a.length) {
+			System.out.println(count++);
 			swap(a, (end % a.length), start % (rotate-1));
-			rotateA(a, rotate, start+1, end+1);
+			rotateA(a, rotate, start+1, end+1, index+1);
 		}
 	}
 	
